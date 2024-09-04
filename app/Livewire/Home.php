1e9333807d89391e5a282f23babe6324250f4693
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-09-05 05:02:39 +0800

    update api page

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -7,17 +7,39 @@ use GuzzleHttp\Client;
 
 class Home extends Component
 {
-
-    public $responseData;
+   
+    public $responseData = [];
     public $imagepath;
-    
+    public $page = 1;
+
     public function mount()
     {
-        $client = new Client();
+        $this->fetchMovies();
+    }
 
-        $response = $client->request('GET', 'https://api.themoviedb.org/3/discover/movie?api_key=' . env('TMBD_API'));
-        $this->responseData = json_decode($response->getBody(), true);
+    public function fetchMovies()
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'https://api.themoviedb.org/3/discover/movie', [
+            'query' => [
+                'api_key' => env('TMBD_API'),
+                'page' => $this->page
+            ]
+        ]);
+        
+        $data = json_decode($response->getBody(), true);
+
+        // Merge new results with existing ones
+        $this->responseData = array_merge_recursive($this->responseData, $data);
         $this->imagepath = "https://image.tmdb.org/t/p/original/";
+    }
+
+    public function nextPage()
+    {
+        if ($this->page < $this->responseData['total_pages']) {
+            $this->page++;
+            $this->fetchMovies();
+        }
     }
 
     public function render()
--- app/Livewire/Movies.php
+++ app/Livewire/Movies.php
@@ -12,18 +12,39 @@ class Movies extends Component
 
     use WithPagination;
 
-    public $responseData ;
+    public $responseData = [];
     public $imagepath;
+    public $page = 1;
     public $search ='';
         
     public function mount()
     {
+        $this->fetchMovies();
+    }
+
+    public function fetchMovies()
+    {
         $client = new Client();
+        $response = $client->request('GET', 'https://api.themoviedb.org/3/discover/tv', [
+            'query' => [
+                'api_key' => env('TMBD_API'),
+                'page' => $this->page
+            ]
+        ]);
+        
+        $data = json_decode($response->getBody(), true);
 
-        $response = $client->request('GET', 'https://api.themoviedb.org/3/discover/tv?api_key=' . env('TMBD_API'));
-        $this->responseData = json_decode($response->getBody(), true);
-
+        // Merge new results with existing ones
+        $this->responseData = array_merge_recursive($this->responseData, $data);
         $this->imagepath = "https://image.tmdb.org/t/p/original/";
+    }
+
+    public function nextPage()
+    {
+        if ($this->page < $this->responseData['total_pages']) {
+            $this->page++;
+            $this->fetchMovies();
+        }
     }
 
     public function render()
